25c87cd359fd39aba15fc520db0f50d549d91d96
Joaquin <ggomezjjoaquin@gmail.com>
2024-06-01 22:15:17 -0300

    Elimino logo de targets

--- pages/blog/tag/_tag.vue
+++ pages/blog/tag/_tag.vue
@@ -12,7 +12,6 @@
 
     <div class="overlay"></div>
     <div class="absolute top-32 left-32 right-32 text-white">
-      <NuxtLink to="/"><Logo /></NuxtLink>
       <div class="mt-16 -mb-3 flex flex-col text-sm">
         <div class="relative lg:w-1/2 xs:w-full xs:h-84 lg:h-full post-left">
           <h1 class="text-4xl font-bold uppercase">
