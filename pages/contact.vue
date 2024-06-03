ca1e3c4e69bfa653dfb26568d6296d570f284a4f
Joaquín <ggomezjjoaquin@gmail.com>
2024-06-03 00:46:03 -0300

    Cambio path en pagina de contacto.

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,15 +3,15 @@
     <div class="container mx-auto p-6 md:p-12 lg:p-20">
       <div class="bg-white dark:bg-gray-800 shadow rounded-lg p-8 md:p-12 lg:p-16 max-w-2xl mx-auto">
         <div class="mb-6">
-          <button
-            @click="goBack"
+          <NuxtLink
+            to="/blog"
             class="flex items-center text-gray-800 dark:text-gray-100 hover:text-blue-600 dark:hover:text-blue-400"
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" class="mr-2">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
             </svg>
             Regresar
-          </button>
+          </NuxtLink>
         </div>
         
         <h1 class="text-4xl font-semibold mb-8 text-center">Contáctame</h1>
