8faa37e9378b73d54989ed8c4c5bfb9dd7f6dc82
Joaquín Gómez <ggomezjjoaquin@gmail.com>
2024-06-30 03:52:06 -0300

    Arreglo estructura de carpetas

--- dist/_nuxt/6b9dc86.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{338:function(t,n,e){"use strict";e.r(n);var l=e(49),component=Object(l.a)({},(function(){var t=this,n=t._self._c;return n("section",[n("h1",{staticClass:"text-4xl"},[t._t("default")],2),t._v(" "),t._t("description")],2)}),[],!1,null,null,null);n.default=component.exports}}]);
