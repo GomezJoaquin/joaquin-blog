b7ff9f75f94e7e8e080bf52b7da7ef75cd7012de
Joaquín Gómez <ggomezjjoaquin@gmail.com>
2024-06-30 02:07:46 -0300

    Quitar articulo.

--- dist/_nuxt/static/1719085987/home/about/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/home/about", {data:[{}],fetch:{},mutations:[]});
---
+++ dist/_nuxt/static/1719724047/blog/tag/reflexiones/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fjoaquin-blog\u002F_nuxt\u002Fstatic\u002F1719724047",layout:"default",error:a,state:{darkMode:false},serverRendered:true,routePath:"\u002Fblog\u002Ftag\u002Freflexiones",config:{_app:{basePath:"\u002Fjoaquin-blog\u002F",assetsPath:"\u002Fjoaquin-blog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4343c793"}}}}(null));
